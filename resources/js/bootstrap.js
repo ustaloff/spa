18557b7990cb2e8663ccdf6db74bad4caa845aaf
Kirils Ustalovs <kirils.u@rdvit.com>
2023-03-18 19:37:30 +0200

    Setup App

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,9 @@
+import loadash from 'lodash';
+window._ = loadash;
+
+import * as Popper from '@popperjs/core';
+window.Popper = Popper;
+
 import 'bootstrap';
 
 /**
@@ -10,6 +16,7 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.withCredentials = true;
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
