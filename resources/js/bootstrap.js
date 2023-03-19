80180825fd97cb1e1a2a371222893524c82861f8
Kirils Ustalovs <kirils.u@rdvit.com>
2023-03-19 10:38:45 +0200

    Final setup

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,7 +1,9 @@
-import loadash from 'lodash';
-window._ = loadash;
+import lodash from 'lodash';
+
+window._ = lodash;
 
 import * as Popper from '@popperjs/core';
+
 window.Popper = Popper;
 
 import 'bootstrap';
@@ -13,10 +15,12 @@ import 'bootstrap';
  */
 
 import axios from 'axios';
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.withCredentials = true;
+//window.axios.defaults.baseURL = "http://localhost:8000";
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
